99142a30b0e597bed1f4da8787c4ff0a5c847d9c
Jones Larry <jones.larry@duke.edu>
2025-03-30 14:26:06 -0400

    Updated NGINX configuration for HTTPS and proxy

--- .platform/hooks/postdeploy/01_reload_nginx.sh
+++ .platform/hooks/postdeploy/01_reload_nginx.sh
@@ -3,8 +3,5 @@ echo "Reloading Nginx..."
 
 set -e
 
-# Append custom NGINX config inclusion
-echo "include /var/app/current/.platform/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf
-
 # Test and restart NGINX
 nginx -t && systemctl restart nginx
