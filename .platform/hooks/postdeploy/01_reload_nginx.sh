7f242e9e14d789e56399024ceaeb67973b2f531e
Jones Larry <jones.larry@duke.edu>
2025-03-30 13:58:23 -0400

    Ensure NGINX loads custom config

--- .platform/hooks/postdeploy/01_reload_nginx.sh
+++ .platform/hooks/postdeploy/01_reload_nginx.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "Reloading Nginx..."
-service nginx reload
+
+set -e
+
+# Append custom NGINX config inclusion
+echo "include /var/app/current/.platform/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf
+
+# Test and restart NGINX
+nginx -t && systemctl restart nginx
